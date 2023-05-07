1b1474fd0ea72169d9798076248a9d814524f81f
Rin Brk <dareka826@gmail.com>
2023-05-07 19:24:32 +0200

    Build blog pages

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 m4 -I ./src ./src/page.m4.html ./src/index.m4.html >./index.html
+
+[ -d ./blog ] || mkdir ./blog
+for POST in ./src/blog/*; do
+    FNAME="${POST##*/}"
+    FNAME="${FNAME%.m4.html}.html"
+
+    m4 -I ./src ./src/page.m4.html "${POST}" >./blog/"${FNAME}"
+done
